60cbdc87dc6ffec68afa02b5dc103ccaff205121
Michael Davenport <mdaven19@byu.edu>
2024-07-29 12:47:25 -0600

    fix client main name

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,7 +5,7 @@ import javax.websocket.DeploymentException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-public class ClientMain {
+public class Main {
     public static void main(String[] args) throws DeploymentException, URISyntaxException, IOException {
         Repl repl = new Repl();
         String host = "localhost";
